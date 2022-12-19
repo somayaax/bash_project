d825953037247ff0fd64097d0eefe778b17a555d
somayaax <88964367+somayaax@users.noreply.github.com>
2022-12-19 20:45:01 +0200

    Add files via upload

--- project/secondaryMenu.sh
+++ project/secondaryMenu.sh
@@ -9,48 +9,7 @@ COLUMNS=8
 select choice in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "back to main menu"
 do
 case $REPLY in 
-1) echo "please enter table name"
-   read name
-   if [ ! -f $name ]
-	then
-	echo "please enter number of columns"
-	read cols
-	re='^[0-9]+$'
-	if ! [[ $cols =~ $re ]]
-	then
-		echo "not a number"
-	else
-		touch $name
-		touch $name.metadata
-	
-		echo "please enter primary key of the table"
-		read primaryKey
-		echo -e "please enter type of pk\nint for integer\nstring for string\nchar for character"
-		read primaryKeyType
-		pk=$primaryKey:$primaryKeyType
-		echo $pk >> $name.metadata
-	
-		let cols=$cols-1
-		i=0
-		j=2
-		while [ $i -lt $cols ]
-		do
-		echo "please enter field ${j} name"
-		read field
-		echo -e "please enter field ${j} type\nint for integer\nstring for string\nchar for character"
-   		read fieldType
-		entry=$field:$fieldType
-		echo $entry >> $name.metadata
-		let i=$i+1
-		let j=$j+1
-		done
-	fi
-
-	else
-	echo "Table already exists"
-   fi
-
-
+1) ../../createTable.sh
 ;;
 2) ls | grep -v .metadata$
 ;;
@@ -64,33 +23,13 @@ case $REPLY in
    echo "table doesn't exist"
    fi
 ;;
-4) ../../insert.sh
+4) ../../insert.sh $1
 ;;
-
 5) ../../select.sh $1
-
-	#echo -e "enter fields\nall $columns"
-	#ans=""
-	#answer=""
-	#read answer
-	#if [ "$answer" = "all" ]
-	#then
-	#awk -F: 'BEGIN{}{print $0}' $tableName
-		
-	#else
-	#while [ "$answer" != "all" ]
-	#do
-	#read answer
-	#ans=$ans" "$answer
-	#done
-	#fi
-
-
-
 ;;
-6) echo "6"
+6) ../../delete.sh
 ;;
-7) echo "7"
+7) ../../update.sh
 ;;
 8) ../../mainmenu.sh
 ;;
