9cca13cf82ca145652ba74da1e09d8852885d4ec
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2022-12-20 15:49:38 +0200

    Add files via upload

--- project/delete.sh
+++ project/delete.sh
@@ -7,7 +7,7 @@ PS3="$tableName>>"
 if [ -f $tableName -a $tableName ]
 then
 
-	select choice in "delete all without condition" "delete a specific row " 
+	select choice in "delete all without condition" "delete by id " "delete record with specific column"
 	do
 	case $REPLY in 
 	#delete all records
@@ -20,7 +20,14 @@ then
 		awk -v VARIABLE=$pk -F ":" '{ if ( $1 != VARIABLE ) print $0 }' $tableName  > temp2
 		mv temp2 $tableName
 	;;
-	esac
+	3) read -p "enter condition : Column name >>  " column
+           read -p "enter condition : Value  >>  " value
+	   columnNumber=`grep -n -w $column $tableName.metadata | cut -d: -f1`
+	   awk -v COL=$columnNumber -v VAL=$value -F ":" '{ if($COL!=VAL){print $0}}' $tableName > temp3
+	   rm $tableName
+	   mv temp3 $tableName
+	;;
+	esac 
 	done
 else 
 echo "table doesn't exist"
