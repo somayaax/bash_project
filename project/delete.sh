ec8e514ccc4323856da471e355f7d8ddc7b58f28
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2022-12-20 15:55:27 +0200

    Add files via upload

--- project/delete.sh
+++ project/delete.sh
@@ -7,7 +7,7 @@ PS3="$tableName>>"
 if [ -f $tableName -a $tableName ]
 then
 
-	select choice in "delete all without condition" "delete by id " "delete record with specific column"
+	select choice in "delete all without condition" "delete record with specific column"
 	do
 	case $REPLY in 
 	#delete all records
@@ -15,12 +15,7 @@ then
 	   rm $tableName
 	   mv temp $tableName
 	;;
-	#delete a specific row
-	2) read -p "enter id of the record you want to delete : " pk
-		awk -v VARIABLE=$pk -F ":" '{ if ( $1 != VARIABLE ) print $0 }' $tableName  > temp2
-		mv temp2 $tableName
-	;;
-	3) read -p "enter condition : Column name >>  " column
+	2) read -p "enter condition : Column name >>  " column
            read -p "enter condition : Value  >>  " value
 	   columnNumber=`grep -n -w $column $tableName.metadata | cut -d: -f1`
 	   awk -v COL=$columnNumber -v VAL=$value -F ":" '{ if($COL!=VAL){print $0}}' $tableName > temp3
