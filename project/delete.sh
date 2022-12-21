e232ae611527f50115f392f7511878f5a242a410
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2022-12-21 20:53:07 +0200

    Add files via upload

--- project/delete.sh
+++ project/delete.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
+echo -e "\n DELETE \n"
 	read -p "Please enter table name: " tableName
-PS3="$tableName>>"
-while [ -f $tableName -a $tableName ] 
+
+while [ ! -f $tableName -a $tableName ] 
 do
-	echo "Table already exist"
+	echo "Table doesn't exist"
 	read -p "Please enter table name: " tableName
 done
 
-select choice in "delete all without condition" "delete record with specific column"
+PS3="Delete FROM $tableName>>"
+select choice in "Delete all records without condition" "Delete record with specific column"
 do
 case $REPLY in 
 #delete all records
 1) head -n 1 $tableName > temp
    rm $tableName
    mv temp $tableName
+   echo " deleted successfully"
 ;;
 2) read -p "enter condition : Column name >>  " column
    read -p "enter condition : Value  >>  " value
+	while [ ! $column ]  || [ ! $value ] 
+        do
+	echo "Fields can't be empty!"
+	read -p "enter condition : Column name >>  " column
+        read -p "enter condition : Value  >>  " value
+	done
    columnNumber=`grep -n -w $column $tableName.metadata | cut -d: -f1`
    awk -v COL=$columnNumber -v VAL=$value -F ":" '{ if($COL!=VAL){print $0}}' $tableName > temp3
+if [ `cat temp3 | wc -l` == 1 ]
+then 
+echo "No records found with this condition"
+else
    rm $tableName
    mv temp3 $tableName
+   echo " deleted successfully"
+fi 
+
+;;
+*) echo "please enter value 1 or 2"
 ;;
 esac 
 done
