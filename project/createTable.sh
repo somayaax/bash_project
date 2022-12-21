e4997c8671f14657613465d99152815be74a2ea4
somayaax <88964367+somayaax@users.noreply.github.com>
2022-12-21 23:04:35 +0200

    Add files via upload

--- project/createTable.sh
+++ project/createTable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "Please enter table name :" name
-while [ -f $name -a ! $name ] 
+while [ -f $name -o ! $name ] 
 do
 	echo "Table already exist"
 	read -p "Please enter table name  :" name
@@ -10,10 +10,22 @@ done
 touch $name
 touch $name.metadata
 read -p  "Please enter number of columns " cols
+while [ ! $cols ]
+do
+	read -p  "Please enter number of columns " cols
+done
+
 read -p  "Please enter primary key of the table " primaryKey
+
+while [ ! $primaryKey ]
+do
+read -p  "Please enter primary key of the table " primaryKey
+done
+
+
 header=$primaryKey
 read -p "please enter type of the primary key [ int-string-float ] " primaryKeyType
-while [ $primaryKeyType != "int" -a $primaryKeyType != "string" -a $primaryKeyType != "float" ]
+while [[ $primaryKeyType != "int" ]] && [[ $primaryKeyType != "string" ]] && [[ $primaryKeyType != "float" ]] || [[ ! $primaryKeyType ]]
 do
 echo -e "\nWrong input ! please enter [ int-string-float ]\n"
 read -p "please enter type of the primary key: " primaryKeyType
@@ -27,21 +39,29 @@ i=0
 j=2
 while [ $i -lt $cols ]
 	do
-	read -p  "please enter field ${j} name :" field
-	columnExists=`cut -d: -f1 $name.metadata | grep -w $field`
-	while [ $columnExists ]
-	do
+	read -p  "please enter field ${j} name :" field	
+	columnExists=`cut -d: -f1 $name.metadata | grep -w $field  2> /dev/null` 
+	while [[ $columnExists ]] || [[ ! $field ]]
+	do	
+		if [ $columnExists ]
+		then 
 		echo "column already exist in table $name"
 		read -p  "please enter field ${j} name :" field
-		columnExists=`cut -d: -f1 $name.metadata | grep -w $field`
+		columnExists=`cut -d: -f1 $name.metadata | grep -w $field 2> /dev/null`
+		elif [  ! $field  ] 
+		then
+	read -p  "please enter field ${j} name :" field	
+	columnExists=`cut -d: -f1 $name.metadata | grep -w $field  2> /dev/null` 
+		fi	
 	done
 	
 	read -p "please enter field ${j} type [ int-string-float ] " fieldType
-	while [ $fieldType != "int" -a $fieldType != "string" -a $fieldType != "float" ]
+
+	while [[ $fieldType != "int" ]] && [[ $fieldType != "string" ]] && [[ $fieldType != "float" ]] || [[ ! $fieldType ]]	
 		do
-		echo "Wrong input ! please enter [ int-string-float ] "
-		read -p "please enter field ${j} type [ int-string-float ] " fieldType
-		done
+		echo -e "\nWrong input ! please enter [ int-string-float ]\n"
+		read -p "please enter field ${j} type: " fieldType
+	done
 	header=$header:$field
 	entry=$field:$fieldType
 	echo  $entry >> $name.metadata
