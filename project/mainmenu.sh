88c3e846a312db3bac6c35552405926683dc37ab
somayaax <88964367+somayaax@users.noreply.github.com>
2022-12-21 18:09:42 +0200

    Add files via upload

--- project/mainmenu.sh
+++ project/mainmenu.sh
@@ -1,43 +1,64 @@
 #!/bin/bash
 function create_Database {
 	read -p "Please enter database name: " name
-	while [ -d ./databases/$name ]
+
+	while [ -d ./databases/$name ] || [ ! $name ]
 	do
-		echo "Database already exist!"
+		if [ ! $name ]
+		then
+			echo -e "\nYou didn't enter a database name\n"
+			
+		else
+			echo -e "\nDatabase already exist!\n"
+		fi
 		read -p "Please enter database name: " name
 	done
+
 	mkdir -p ./databases/$name
 	echo database created successefuly
 }
 
-function check_Database {
-	read -p "Please enter database name: " name
-	while [ ! $name ]
-	do
-	echo "You didn't enter a database name"
-	read -p "Please enter database name: " name
-	done
-	while [ ! -d ./databases/$name ]
-	do
-		echo "Database not found"
-		read -p "Please enter database name: " name
-	done
-echo $name
-}
 COLUMNS=4
 select x in 'create Database' 'list Databases' 'connect to Database' 'Drop Database'
 do
 case $REPLY in 
 1) create_Database
 ;;
-2) ls ./databases 
+2) 
+	if [ -e ./databases ] 
+	then 
+		ls ./databases
+	else
+		echo -e "\nNo Databases found!\n"
+	fi
+	 
 ;;
 3) 
-   name=$(check_Database)
-   gnome-terminal --title=$name -e "./secondaryMenu.sh $name" > /dev/null  
+   	read -p "Please enter database name: " name
+	while [ ! -d ./databases/$name ] || [ ! $name ]
+	do
+		if [ ! -d ./databases/$name ]
+		then
+			echo -e "\nDatabase not found\n"
+		else
+			echo -e "\nYou didn't enter a database name\n"
+		fi
+		read -p "Please enter database name: " name
+	done
+	gnome-terminal --title=$name -e "./secondaryMenu.sh $name" > /dev/null  
 ;;
 4)
-   name=$(check_Database)
+   	read -p "Please enter database name: " name
+	while [ ! -d ./databases/$name ] || [ ! $name ]
+	do
+		if [ ! -d ./databases/$name ]
+		then
+		echo -e "\nDatabase not found\n"
+		else
+		echo -e "\nYou didn't enter a database name\n"
+		fi
+		read -p "Please enter database name: " name
+	done
    rm -r ./databases/$name
    echo "database dropped successfully"
 ;;
